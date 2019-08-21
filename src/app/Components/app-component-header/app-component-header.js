97af9252c17c9bd7b5f42527237065fa49aacc42
hubert.wozniak977@gmail.com <hubert.wozniak977@gmail.com>
2019-08-21 12:48:46 +0200

    menu-hamburger

--- src/app/Components/app-component-header/app-component-header.js
+++ src/app/Components/app-component-header/app-component-header.js
@@ -3,8 +3,22 @@ import './app-component-header.scss';
 import logo from '../../../assets/hills.png';
 import video from '../../../assets/blue.mp4';
 class Header extends React.Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = {ShowMenu: false}
+        this.ShowMenu = this.ShowMenu.bind(this);
+    }
+    ShowMenu()
+    {
+        this.setState({
+            ShowMenu: !this.state.ShowMenu
+          });
+        console.log(this.state.ShowMenu);
+    }
     render()
     {
+        const isActive = this.state.ShowMenu ? 'is-active' : '';
         return(
             <div>
         <header>
@@ -13,11 +27,11 @@ class Header extends React.Component {
                     <img src={logo} className="Waterfall-image" />
             </section>
             <nav className="Menu">
-                <ul>
-                <li>Home</li>   
-                <li>Article</li>
-                <li>Contact</li>   
-                </ul> 
+            <button className={`hamburger hamburger--collapse ${isActive}`} type="button" onClick={this.ShowMenu}>
+            <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+            </span>
+            </button>
             </nav>
         </header>
          <video autoPlay muted loop id="waterfall" className="waterfall">
